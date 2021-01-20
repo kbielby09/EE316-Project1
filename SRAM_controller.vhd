723e6d4761d53167445198841beaa1a5ef47ec6f
kbielby09 <63510934+kbielby09@users.noreply.github.com>
2021-01-20 16:47:52 -0500

    added 1 hz clock and address counter for SRAM controller

--- SRAM_controller.vhd
+++ SRAM_controller.vhd
@@ -48,7 +48,7 @@ port
                                                             -- bits 0-3: digit 1; bits 3-7: digit 2, bit 7: digit 4
 
  -- hex display for data address
- OUT_DATA_ADR : out std_logic_vector(15 downto 0)      -- segments that are to be illuminated for the seven segment hex
+ OUT_DATA_ADR : out std_logic_vector(7 downto 0)      -- segments that are to be illuminated for the seven segment hex
                                                             -- digits that are being used to display the address
 
   );
@@ -64,10 +64,18 @@ architecture rtl of SRAM_controller is
   -- SIGNALS --
   -------------
 
+  -- counter signal for 1 Hz clock
+  signal one_hz_counter : unsigned(25 downto 0) := "00000000000000000000000000";
+  signal count_enable : std_logic;
+
   -- contains address that data is written to
-  signal input_data_addr : std_logic_vector(7 downto 0);
+  signal input_data_addr : unsigned(7 downto 0);
 
   signal input_data      : std_logic_vector(15 downto 0);
+
+  signal write_enable    : std_logic;
+  signal output_enable   : std_logic;
+  signal                 : std_logic;
 
   -- counter for digit refresh
   signal digit_refresh_counter : unsigned(16 downto 0) := "00000000000000000";
@@ -84,27 +92,58 @@ architecture rtl of SRAM_controller is
 
 begin
 
-  SRAM_COUNTER : process (I_CLK_50MHZ)
+  ONE_HZ_CLOCK : process (I_CLK_50MHZ, MEM_RESET)
       begin
+		   if(MEM_RESET = '1') then
+			    -- TODO add reset code here
+			elsif (rising_edge(I_CLK_50MHZ)) then
+				  one_hz_counter <= one_hz_counter + 1;
+		      if (one_hz_counter = "10111110101111000001111111") then  -- check for 1 Hz clock (count to 50 million)
+				      count_enable <= '1';
+              one_hz_counter <= (others => '0');
+				  else
+				      count_enable <= '0';
+		      end if;
+			end if;
 
-  end process SRAM_COUNTER;
+  end process ONE_HZ_CLOCK;
 
-  INITIALIZE_SRAM : process (I_CLK_50MHZ, INITIALIZE, RESET)
-      begin
-        if(INITIALIZE = '1' or RESET = '1') then
+   SRAM_COUNTER : process (I_CLK_50MHZ, MEM_RESET)
+       begin
+           if(rising_edge(I_CLK_50MHZ)) then
+               if(one_hz_counter = "10111110101111000001111111") then
+                 if(input_data_addr = "11111111" and count_enable = '0') then
+                     input_data_addr <= "00000000";  -- reset address count
+                else
+                    input_data_addr <= input_data_addr + 1;  -- increase count
+                 end if;
+               end if;
+           end if;
+   end process SRAM_COUNTER;
 
-        end if;
+ -- INITIALIZE_SRAM : process (I_CLK_50MHZ, INITIALIZE, RESET)
+ --     begin
+ --       if(INITIALIZE = '1' or RESET = '1') then
+ --
+ --       end if;
+ --
+ -- end process INITIALIZE_SRAM;
 
-  end process INITIALIZE_SRAM;
+ WRITE_SRAM_DATA : process (I_CLK_50MHZ, WE, OE, IN_DATA)
+     begin
+         if(WE = '0' and CE = '1') then
+             input_data <= IN_DATA;
+         end if;
+ end process WRITE_SRAM_DATA;
 
-  WRITE_SRAM_DATA : process (I_CLK_50MHZ, WE, OE, IN_DATA)
-      begin
-          if(WE = '0' and CE = '1') then
-              input_data <= IN_DATA;
-          end if;
-  end process WRITE_SRAM_DATA;
+ READ_SRAM_DATA : process ()
+     begin
+         if(rising_edge(I_CLK_50MHZ)) then
 
-  OUT_DATA_ADR <= input_data_addr;
+         end if;
+ end process READ_SRAM_DATA;
+
+  OUT_DATA_ADR <= std_logic_vector(input_data_addr);
   OUT_DATA     <= input_data;
 
 
