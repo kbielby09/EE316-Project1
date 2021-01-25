50872742403c68b123c4bab4c1cf8da9987bad7c
Joseph Drahos <56799491+JosephDrahos@users.noreply.github.com>
2021-01-25 18:35:07 -0500

    Added input and output address signals

--- quad_hex_driver.vhd
+++ quad_hex_driver.vhd
@@ -8,6 +8,8 @@ port
   I_CLK_50MHZ         : in Std_logic;
   I_RESET_N           : in Std_logic;
   I_COUNT             : in Std_logic_Vector(15 downto 0);
+  I_DATA_ADDR         : in Std_logic_Vector(7 downto 0);
+  O_DATA_ADDR	      : out Std_logic_Vector(13 downto 0);
   O_HEX_N             : out Std_logic_Vector(27 downto 0)
 );
 end;
@@ -15,6 +17,7 @@ end;
 architecture behavioral of quad_hex_driver is
 
   signal O_HEX_N_BUF: std_logic_vector(27 downto 0);
+  signal O_DATA_ADDR_BUF: std_logic_vector(13 downto 0);
 
   begin
 quad_hex_driver: process(I_CLK_50MHZ, I_RESET_N) begin 
@@ -172,7 +175,81 @@ quad_hex_driver: process(I_CLK_50MHZ, I_RESET_N) begin
           O_HEX_N_BUF <= O_HEX_N_BUF and "1111111000000000000000000000";
       end case;	
       
+		case(I_DATA_ADDR(3 downto 0)) is
+        when "0000"             => -- '0'
+          O_DATA_ADDR_BUF <= "00000001000000";
+        when "0001"             => -- '1'
+          O_DATA_ADDR_BUF <= "00000001111001";
+        when "0010"             => -- '2'
+          O_DATA_ADDR_BUF <= "00000000100100";
+        when "0011"             => -- '3'
+          O_DATA_ADDR_BUF <= "00000000110000";
+        when "0100"             => -- '4'
+          O_DATA_ADDR_BUF <= "00000000011001";
+        when "0101"             => -- '5'
+          O_DATA_ADDR_BUF <= "00000000010010";
+        when "0110"             => -- '6'
+          O_DATA_ADDR_BUF <= "00000000000010";
+        when "0111"             => -- '7'
+          O_DATA_ADDR_BUF <= "00000001111000";
+        when "1000"             => -- '8'
+          O_DATA_ADDR_BUF <= "00000000000000";
+        when "1001"             => -- '9'
+          O_DATA_ADDR_BUF <= "00000000010000";
+        when "1010"             => -- 'A'
+          O_DATA_ADDR_BUF <= "00000000001000";
+        when "1011"             => -- 'B'
+          O_DATA_ADDR_BUF <= "00000000000011";
+        when "1100"             => -- 'C'
+          O_DATA_ADDR_BUF <= "00000001000110";
+        when "1101"             => -- 'D'
+          O_DATA_ADDR_BUF <= "00000000100001";
+        when "1110"             => -- 'E'
+          O_DATA_ADDR_BUF <= "00000000000110";
+        when "1111"             => -- 'F'
+          O_DATA_ADDR_BUF <= "00000000001110";
+        when others             => -- ' '
+          O_DATA_ADDR_BUF <= "00000001111111";
+      end case; 
+		
+		case(I_DATA_ADDR(7 downto 4)) is
+        when "0000"             => -- '0'
+          O_DATA_ADDR_BUF <= O_DATA_ADDR_BUF and "10000000000000";
+        when "0001"             => -- '1'
+          O_DATA_ADDR_BUF <= O_DATA_ADDR_BUF and "11110010000000";
+        when "0010"             => -- '2'
+          O_DATA_ADDR_BUF <= O_DATA_ADDR_BUF and "01001000000000";
+        when "0011"             => -- '3'
+          O_DATA_ADDR_BUF <= O_DATA_ADDR_BUF and "01100000000000";
+        when "0100"             => -- '4'
+          O_DATA_ADDR_BUF <= O_DATA_ADDR_BUF and "00110010000000";
+        when "0101"             => -- '5'
+          O_DATA_ADDR_BUF <= O_DATA_ADDR_BUF and "00100100000000";
+        when "0110"             => -- '6'
+          O_DATA_ADDR_BUF <= O_DATA_ADDR_BUF and "00000100000000";
+        when "0111"             => -- '7'
+          O_DATA_ADDR_BUF <= O_DATA_ADDR_BUF and "11110000000000";
+        when "1000"             => -- '8'
+          O_DATA_ADDR_BUF <= O_DATA_ADDR_BUF and "00000000000000";
+        when "1001"             => -- '9'
+          O_DATA_ADDR_BUF <= O_DATA_ADDR_BUF and "00100000000000";
+        when "1010"             => -- 'A'
+          O_DATA_ADDR_BUF <= O_DATA_ADDR_BUF and "00010000000000";
+        when "1011"             => -- 'B'
+          O_DATA_ADDR_BUF <= O_DATA_ADDR_BUF and "00000110000000";
+        when "1100"             => -- 'C'
+          O_DATA_ADDR_BUF <= O_DATA_ADDR_BUF and "10001100000000";
+        when "1101"             => -- 'D'
+          O_DATA_ADDR_BUF <= O_DATA_ADDR_BUF and "01000010000000";
+        when "1110"             => -- 'E'
+          O_DATA_ADDR_BUF <= O_DATA_ADDR_BUF and "00001100000000";
+        when "1111"             => -- 'F'
+          O_DATA_ADDR_BUF <= O_DATA_ADDR_BUF and "00011100000000";
+        when others             => -- ' '
+          O_DATA_ADDR_BUF <= O_DATA_ADDR_BUF and "11111110000000";
+      end case; 
       O_HEX_N <= O_HEX_N_BUF;
+		O_DATA_ADDR <= O_DATA_ADDR_BUF;
       
     end if;
   end process quad_hex_driver;
